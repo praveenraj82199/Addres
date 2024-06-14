7f3be9b4e02e7955abbee64b9b76217b6467f1b1
praveenraj82199 <rajpraveen648@gmail.com>
2024-06-14 18:37:27 +0530

    new file add

---
+++ src/org/add/Tech.java
@@ -0,0 +1,13 @@
+package org.add;
+
+public class Tech {
+	
+	public static void main (String[]args) {
+		System.out.println("praveen");
+	
+				
+		
+	}
+
+}
+ // test paper 03 question no; 05 (method over load)
